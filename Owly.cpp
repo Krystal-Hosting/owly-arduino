4e003077a51d6671033e32e29ed5dd528a9f8e22
Josh Bonfield <josh@b-digital.biz>
2015-10-06 13:35:19 +0100

    Beta V1 - working on the Yun.

---
+++ Owly.cpp
@@ -0,0 +1,32 @@
+#include "Arduino.h"
+#include "Owly.h"
+#include "libs/PubSubClient/PubSubClient.h"
+#include "libs/PubSubClient/PubSubClient.cpp"
+#include "Client.h"
+
+PubSubClient _pubsubclient();
+
+Owly::Owly(const char *inputuser_username, const char *inputuser_password, Client& client)
+{
+    this->_inputuser_username = inputuser_username;
+    this->_inputuser_password = inputuser_password;
+    this->_clientid = inputuser_username; // TODO Should be changed to a random string.
+    
+    _pubsubclient.setClient(client);
+    _pubsubclient.setServer("mqtt.owly.uk", 1883);
+}
+
+void Owly::Publish(char* topic, char* payload)
+{
+     _pubsubclient.publish(topic, payload);
+}
+
+boolean Owly::Connect()
+{
+    return _pubsubclient.connect(this->_clientid, this->_inputuser_username, this->_inputuser_password);
+}
+
+void Owly::Loop()
+{
+    _pubsubclient.loop();
+}
---
+++ Owly.h
@@ -0,0 +1,24 @@
+#ifndef Owly_h
+#define Owly_h
+
+#include "Arduino.h"
+#include "libs/PubSubClient/PubSubClient.h"
+#include "Client.h"
+
+class Owly
+{
+    public:
+        Owly(const char *inputuser_username, const char *inputuser_password, Client& client);
+        boolean Connect();
+        void Loop();
+        void Publish(char* topic, char* payload);
+        void Blink(int time);
+    
+    private:
+        const char *_inputuser_username;
+        const char *_inputuser_password;
+        const char *_clientid;
+        PubSubClient _pubsubclient;
+};
+
+#endif
